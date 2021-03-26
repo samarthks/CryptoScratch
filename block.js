6294d02b9f2d26537b1e7007e1416d23eddcb7d4
samarthks <samarthk0105@gmail.com>
2021-03-26 01:40:10 -0400

    block object made

--- block.js
+++ block.js
@@ -1,21 +1,26 @@
 const { GENESIS_DATA } = require("./config");
+const cryptoHash = require("./crypto-hash");
+
 class Block {
-    constructor({ timestamp, lastHash, hash, data }) {
-      this.timestamp = timestamp;
-      this.lastHash = lastHash;
-      this.hash = hash;
-      this.data = data;
-    }
-   static genesis(){
-return new this(GENESIS_DATA); //this() keyword represent Block instance
-    }
-    static mineBlock({lastBlock,data}){
-      return new this({
-        timestamp:Date.now(),
-        lastHash:lastBlock.hash,
-        data
-      });
-    }
+  constructor({ timestamp, lastHash, hash, data }) {
+    this.timestamp = timestamp;
+    this.lastHash = lastHash;
+    this.hash = hash;
+    this.data = data;
   }
-  
-  module.exports=Block;
+  static genesis() {
+    return new this(GENESIS_DATA); //this() keyword represent Block instance
+  }
+  static mineBlock({ lastBlock, data }) {
+    const timestamp = Date.now();
+    const lastHash = lastBlock.hash;
+    return new this({
+      timestamp,
+      lastHash,
+      data,
+      hash: cryptoHash(timestamp, lastHash, data),
+    });
+  }
+}
+
+module.exports = Block;
