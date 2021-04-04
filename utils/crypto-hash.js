ed67f344e7611bad903a25cae2673f4ba87754cd
samarthks <samarthk0105@gmail.com>
2021-04-04 00:52:01 -0400

    cryptohash function modified

--- utils/crypto-hash.js
+++ utils/crypto-hash.js
@@ -1,8 +1,14 @@
 const crypto = require("crypto");
 //const hexToBinary=require('hex-to-binary');
-const cryptoHash = (...inputs) => { //... spread operator for multiple arguments
+const cryptoHash = (...inputs) => {
+  //... spread operator for multiple arguments
   const hash = crypto.createHash("sha256");
-  hash.update(inputs.sort().join(" "));//to have argument in 1 order
-  return (hash.digest("hex"));
+  hash.update(
+    inputs
+      .map((input) => JSON.stringify(input))
+      .sort()
+      .join(" ")
+  ); //to have argument in 1 order
+  return hash.digest("hex");
 };
 module.exports = cryptoHash;
